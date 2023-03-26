14bde13d109fc7c1c94dab906c6340d8f5f3abbb
radearsr <radea.surya1@gmail.com>
2023-03-27 05:46:31 +0700

    update form validation when type all

--- assets/js/pages/grabingPage.js
+++ assets/js/pages/grabingPage.js
@@ -3,6 +3,7 @@ const moreOptions = document.querySelectorAll(".more-option");
 const title = document.querySelector(".title-more-option");
 const findGrabButton = document.querySelector("#find-grab");
 const formFinder = document.querySelector("#form-finder");
+const selectUrlGrabbing = document.querySelector("#url-grabing");
 let currentMoreOption;
 
 tipeGrabbing.addEventListener("change", (event) => {
@@ -22,12 +23,47 @@ tipeGrabbing.addEventListener("change", (event) => {
 
 formFinder.addEventListener("submit", (event) => {
   event.preventDefault();
+  const sourceGrabName = selectUrlGrabbing.getAttribute("name");
+  const sourceGrabLabel = selectUrlGrabbing.parentElement.querySelector("label");
+  const sourceGrabSpan = selectUrlGrabbing.parentElement.querySelector("span");
+
+  const payload = {};
+
+  if (selectUrlGrabbing.value === "Pilih") {
+    sourceGrabLabel.classList.add("text-danger");
+    sourceGrabSpan.classList.remove("d-none");
+    sourceGrabSpan.classList.add("text-danger");
+    selectUrlGrabbing.classList.add("is-invalid");
+  } else {
+    sourceGrabLabel.classList.remove("text-danger");
+    sourceGrabSpan.classList.add("d-none");
+    sourceGrabSpan.classList.remove("text-danger");
+    selectUrlGrabbing.classList.remove("is-invalid");
+    payload[sourceGrabName] = selectUrlGrabbing.value;
+  }
+
   moreOptions.forEach((moreOption) => {
     if (!moreOption.classList.contains("d-none")) {
       const selected = moreOption.querySelectorAll("select");
       selected.forEach((selects) => {
-        console.log(selects.value);
+        const name = selects.getAttribute("name");
+        const labelTag = selects.parentElement.querySelector("label");
+        const spanTag = selects.parentElement.querySelector("span");
+        if (selects.value === "Pilih") {
+          labelTag.classList.add("text-danger");
+          spanTag.classList.remove("d-none");
+          spanTag.classList.add("text-danger");
+          selects.classList.add("is-invalid");
+        } else {
+          labelTag.classList.remove("text-danger");
+          spanTag.classList.add("d-none");
+          spanTag.classList.remove("text-danger");
+          selects.classList.remove("is-invalid");
+          payload[name] = selects.value;
+        }
       });
     }
   });
+
+  console.log(payload);
 });
